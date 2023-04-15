d62b7127293d81e2f6e409723b4eff0118ab99be
rholdxz <rholdxz@gmail.com>
2023-04-16 01:59:20 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,8 +3,8 @@
     # annual rate of interest and time period in years.
     # Do not use this in production. Sample purpose only.
     # Author: Upkar Lidder (IBM)
-    # Additional Authors:
-    # <your GitHub username>
+    # Additional Authors: Roldan Lobitaña Jr.
+    
     # Input:
     # p, principal amount
     # t, time period in years
